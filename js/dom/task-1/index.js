d09d680fc9e6908b3822f0b6a43d3be629511087
akenford <danyamarow69@gmail.com>
2023-07-04 11:02:18 +0700

    dom

---
+++ js/dom/task-1/index.js
@@ -0,0 +1,6 @@
+/* 1. Нужно создать следующую структуру с использованием DOM методов: appendChild, createElement, createTextNode, setAttribute:*/
+<div>
+    Привет, <span style="font-size:150%">Лена</span>
+</div>
+
+/* 2. Найти на странице все <b> и изменить их размер на 15px. Использовать getElementsByTagName, setAttribute */
