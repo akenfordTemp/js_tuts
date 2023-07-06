2017847ce5438a8c143634164b58c74e1edef206
OleksandRiaboshapko <morozov2332@gmail.com>
2023-07-06 15:40:22 +0700

    objects-string-task-3

--- js/objects/string/task-3/index.js
+++ js/objects/string/task-3/index.js
@@ -2,3 +2,9 @@
 Дана строка 'js'. Сделайте из нее строку 'JS'.
 Дана строка 'JS'. Сделайте из нее строку 'js'.
  */
+const strToLower = 'JS';
+alert(strToLower.toLowerCase());
+
+const strToUpper = 'js';
+alert(strToUpper.toUpperCase());
+
