1853bd31c2c4130b316d428aefdc111d6358e018
OleksandRiaboshapko <morozov2332@gmail.com>
2023-07-09 20:11:10 +0700

    objects-string-task-5

--- js/objects/string/task-5/index.js
+++ js/objects/string/task-5/index.js
@@ -2,3 +2,8 @@
 Преобразуйте строку 'var_test_text' в 'varTestText'. Скрипт, конечно же, должен
 работать с любыми аналогичными строками.
  */
+function camelize(str) {
+    return str.split('_').map((word, index) => index === 0 ? word : word[0].toUpperCase() + word.slice(1)).join('');
+}
+console.log(camelize('var_test_text'));
+
