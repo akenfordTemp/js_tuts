a86edf9c90419f90e8ed36d8f1031ecf4cbfc031
akenford <danyamarow69@gmail.com>
2023-07-17 21:52:31 +0700

    fix

--- js/css/task-1/index.js
+++ js/css/task-1/index.js
@@ -1,3 +1,3 @@
 /*
-1. Чем opacity отличается от visible: hidden и что это такое, отличие от overflow: hidden?
+1. Чем opacity отличается от visible: hidden и display: none что это такое, отличие от overflow: hidden?
  */
--- js/dom/task-1/index.js
+++ js/dom/task-1/index.js
@@ -2,5 +2,3 @@
 <div>
     Привет, <span style="font-size:150%">Лена</span>
 </div>
-
-/* 2. Найти на странице все <b> и изменить их размер на 15px. Использовать getElementsByTagName, setAttribute */
